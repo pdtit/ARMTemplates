eda76511aba06a81c3d9dd44d3503d82c2dcda5f
pdetender <peter@pdtit.be>
2018-06-22 05:36:02 +0800

    v1.3
    update install-

--- AzADEMS/vmextensions/deploy-iis-full.ps1
+++ AzADEMS/vmextensions/deploy-iis-full.ps1
@@ -1,1 +1,9 @@
-install-WindowsFeature -Name "Web-Server" -IncludeAllSubFeature
+param($domain, $password)
+
+$smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
+
+Install-WindowsFeature -Name "Web-Server" `
+                       -IncludeManagementTools `
+                       -IncludeAllSubFeature  
+
+Install-WindowsFeature -Name "Web-Mgmt-Tools"
