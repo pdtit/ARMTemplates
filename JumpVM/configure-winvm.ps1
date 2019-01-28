2770f463bf188d3a215721e2b34a6b79081f1c3b
pdtit <peter@pdtit.be>
2019-01-28 17:23:41 +0100

    new config for JumpVM

---
+++ JumpVM/configure-winvm.ps1
@@ -0,0 +1,144 @@
+# Disable IE Enhanced Security Configuration
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+
+New-Item -Path $adminKey -Force
+New-Item -Path $UserKey -Force
+New-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+New-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+
+# Allow file downloads
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+
+# Allow file downloads
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+Set-ItemProperty -Path $HKLM -Name "1807" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1807" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+Set-ItemProperty -Path $HKLM -Name "1807" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1807" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+Set-ItemProperty -Path $HKLM -Name "1807" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1807" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+Set-ItemProperty -Path $HKLM -Name "1807" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1807" -Value 0
+
+# Allow file downloads
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+Set-ItemProperty -Path $HKLM -Name "1604" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1604" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+Set-ItemProperty -Path $HKLM -Name "1604" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1604" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+Set-ItemProperty -Path $HKLM -Name "1604" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1604" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+Set-ItemProperty -Path $HKLM -Name "1604" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1604" -Value 0
+
+# allow websites to open windows without address or status bars 
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+Set-ItemProperty -Path $HKLM -Name "2104" -Value 0
+Set-ItemProperty -Path $HKCU -Name "2104" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+Set-ItemProperty -Path $HKLM -Name "2104" -Value 0
+Set-ItemProperty -Path $HKCU -Name "2104" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+Set-ItemProperty -Path $HKLM -Name "2104" -Value 0
+Set-ItemProperty -Path $HKCU -Name "2104" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+Set-ItemProperty -Path $HKLM -Name "2104" -Value 0
+Set-ItemProperty -Path $HKCU -Name "2104" -Value 0
+
+$HKLM = "HKLM:\Software\Microsoft\Internet Explorer\Security"
+New-ItemProperty -Path $HKLM -Name "DisableSecuritySettingsCheck" -Value 1 -PropertyType DWORD
+Set-ItemProperty -Path $HKLM -Name "DisableSecuritySettingsCheck" -Value 1
+Stop-Process -Name Explorer
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+
+# Allow programmatic clipboard access
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+Set-ItemProperty -Path $HKLM -Name "1407" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1407" -Value 0
+
+
+# Install Chrome
+$Path = $env:TEMP; 
+$Installer = "chrome_installer.exe"
+Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
+Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
+Write-Host "Installing Chrome Browser" -ForegroundColor Green
+
+
+
+# Install Thinfinity RDP
+$Path = $env:TEMP; 
+$Installer = "Thinfinity_Remote_Desktop_Workstation_Setup_x64.msi"
+Invoke-WebRequest "http://www.cybelesoft.com/downloads/Thinfinity_Remote_Desktop_Workstation_Setup_x64.msi" -Outfile $Path\$Installer
+$installfile = "$path\$installer"
+msiexec /i $installfile /quiet /qn /log $path\thinlog.log
+Write-Host "Installing Thinfinity RDP" -ForegroundColor Green
+
+
+
+# Copy Thinfinity Configfile
+$Path = $env:TEMP; 
+$zipfile = "Cybele_Software.zip"
+Invoke-WebRequest "https://pdtitlabsstorage.blob.core.windows.net/templates/jumpvm/Cybele_Software.zip" -Outfile $Path\$zipfile
+$file = "$path\$zipfile"
+expand-archive $file -destinationpath c:\programdata -force
+
+#Start-process -FIlePath "c:\program files\thinfinity\Remote Desktop Workstation\Thinfinity.RemoteDesktop.Workstation.exe"
+
+#Replace config settings for Thinfinity
+#$configpath = "c:\programdata\Cybele Software\Thinfinity\Remote Desktop Workstation\Thinfinity.RemoteDesktop.Workstation.ini"
+
+#((get-content -path $configpath -raw) -replace '8081','443') | set-content -path $configpath
+#((get-content -path $configpath -raw) -replace 'type=Digest','type=None') | set-content -path $configpath
+#((get-content -path $configpath -raw) -replace 'NtlmUsers=','NtlmUsers=jumpvm\labadmin') | set-content -path $configpath
+#((get-content -path $configpath -raw) -replace 'type=Digest','type=NTLM') | set-content -path $configpath
+#((get-content -path $configpath -raw) -replace 'VNC=1','VNC=0') | set-content -path $configpath
+#((get-content -path $configpath -raw) -replace 'FT=1','FT=0') | set-content -path $configpath
+
+
+
+
+#restart-service -name "RemoteDesktopWorkstationSvc"
+
+#Write-Host "Reconfiguring Thinfinity RDP" -ForegroundColor Green
+
+
+
+
+
+
