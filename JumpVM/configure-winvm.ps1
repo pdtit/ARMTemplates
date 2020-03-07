4879d3fafd80d8d57f794a6ccb58d58c3b8b36ce
pdtit <peter@pdtit.be>
2020-03-07 22:12:18 +0100

    Update configure-winvm.ps1
    Updated with Choco

--- JumpVM/configure-winvm.ps1
+++ JumpVM/configure-winvm.ps1
@@ -157,7 +157,6 @@ $workdir = $env:TEMP;
 $ChromeInstaller = "chrome_installer.exe"
 Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $workdir\$ChromeInstaller
 Start-Process -FilePath $workdir\$ChromeInstaller -Args "/silent /install" -Verb RunAs -Wait
-Write-Host "Installing Chrome Browser" -ForegroundColor Green
 
 
 # Silent install Adobe Reader DC
@@ -165,7 +164,51 @@ $workdir = $env:TEMP;
 $adobesource = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1502320053/AcroRdrDC1502320053_en_US.exe"
 $adobedestination = "$workdir\adobeDC.exe"
 Invoke-WebRequest $adobesource -OutFile $adobedestination
-
-# Start the installation
-
 Start-Process -FilePath "$workdir\adobeDC.exe" -ArgumentList "/sPB /rs"
+
+# Silent install Azure CLI
+$workdir = $env:TEMP;
+$azureclisource = "https://aka.ms/installazurecliwindows"
+$azureclidestination = "$workdir\azurecli.msi"
+Invoke-WebRequest -Uri $azureclisource -OutFile $azureclidestination 
+Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+
+# Silent install AZ PowerShell Module
+Install-Module -Name Az -Repository PSGallery -Force
+
+# Silent install Azure Storage Explorer
+$workdir = $env:TEMP;
+$azstoxinstaller = "StorageExplorer.exe"
+$azurestoxsource = "https://go.microsoft.com/fwlink/?LinkId=708343&clcid=0x409"
+$azurestoxdestination = "$workdir\storageexplorer.exe"
+Invoke-WebRequest -Uri $azurestoxsource -OutFile $azurestoxdestination 
+Start-Process -FilePath $workdir\$azstoxinstaller -Args "/silent /install" -Verb RunAs -Wait
+
+# Silent install Docker Desktop
+$workdir = $env:TEMP;
+$dockerinstaller = "Docker Desktop Explorer.exe"
+$dockersource = "https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe"
+$dockerdestination = "$workdir\Docker Desktop Explorer.exe"
+Invoke-WebRequest -Uri $dockersource -OutFile $dockerdestination 
+Start-Process -FilePath $workdir\$dockerinstaller -Args "install --quiet" -Verb RunAs -Wait
+
+
+# Silent install MarkdownPad
+$workdir = $env:TEMP;
+$markdownreaderinstaller = "markdownpad2-setup.exe"
+$markdownreadersource = "http://markdownpad.com/download/markdownpad2-setup.exe"
+$markdownreaderdestination = "$workdir\markdownpad2-setup.exe"
+Invoke-WebRequest -Uri $markdownreadersource -OutFile $markdownreaderdestination 
+Start-Process -FilePath $workdir\$markdownreaderinstaller -Args "/exenoui /install" -Verb RunAs -Wait
+
+# Silent install Chocolatey packager
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# silent install 7zip 
+choco install 7zip
+choco install putty.install
+choco install sql-server-management-studio
+choco install nodejs-lts
+choco install microsoft-edge
+
+
