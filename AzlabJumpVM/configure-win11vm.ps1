43e140d3de31c74e00ebffc62e258e94944c3f07
Peter De Tender <peter@pdtit.be>
2023-10-26 11:41:43 -0700

    Create configure-win11vm.ps1

---
+++ AzlabJumpVM/configure-win11vm.ps1
@@ -0,0 +1,11 @@
+
+
+# Install Chrome
+$workdir = $env:TEMP;
+$ChromeInstaller = "chrome_installer.exe"
+Invoke-WebRequest "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B0022FD8E-83B2-A4FB-5279-5DF9E9769D28%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe" -OutFile $workdir\$ChromeInstaller
+Start-Process -FilePath $workdir\$ChromeInstaller -Args "/silent /install" -Verb RunAs -Wait
+Write-Host "Installing Chrome Browser" -ForegroundColor Green
+
+
+## updates
