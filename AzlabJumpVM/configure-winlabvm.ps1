45f40df716d98770855e99230351950b05feaa85
Peter De Tender <peter@pdtit.be>
2023-10-28 10:45:37 -0700

    Update configure-winlabvm.ps1

--- AzlabJumpVM/configure-winlabvm.ps1
+++ AzlabJumpVM/configure-winlabvm.ps1
@@ -2,12 +2,6 @@
 
 # Set Chocolatey version to 1.4.0 to avoid installing .NET 4.8 Framework (which the newer 2.0 needs, but requires reboot, and blocks CustomScriptExtension
 
-# Install Chrome
-$workdir = $env:TEMP;
-$ChromeInstaller = "chrome_installer.exe"
-Invoke-WebRequest "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B0022FD8E-83B2-A4FB-5279-5DF9E9769D28%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe" -OutFile $workdir\$ChromeInstaller
-Start-Process -FilePath $workdir\$ChromeInstaller -Args "/silent /install" -Verb RunAs -Wait
-Write-Host "Installing Chrome Browser" -ForegroundColor Green
 $env:chocolateyVersion = '1.4.0'
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
