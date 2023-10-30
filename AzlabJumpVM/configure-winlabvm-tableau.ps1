323b3942bbf49768d4be4f02e463c0eacec3a6d9
Peter De Tender <peter@pdtit.be>
2023-10-30 10:10:44 -0700

    Update configure-winlabvm-tableau.ps1

--- AzlabJumpVM/configure-winlabvm-tableau.ps1
+++ AzlabJumpVM/configure-winlabvm-tableau.ps1
@@ -13,34 +13,5 @@ choco feature enable -n allowGlobalConfirmation
 # install Google Chrome
 choco install googlechrome
 
-# install Cassandra
-choco install apache-cassandra
-
-# install python 3.10
-choco install python310
-
-# install PostgreSQL
-choco install postgresql15 --params '/Password:P@55w.rdP@55w.rd /Port:5433' --ia '--enable-components server,commandlinetools'
-
-# install Anaconda Distribution (Python 3.x) 2023.9.0
-choco install anaconda3
-
-# install Neo4j Community Edition 3.5.1
-choco install openjdk
-choco install eclipse-standard-kepler
-choco install neo4j-community
-
-# install Podman 
-choco install podman-machine
-choco install podman-desktop
-choco install podman-cli
-
-$ docker desktop
-choco install docker-desktop
-
-#install Docker on Windows Server
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
-.\install-docker-ce.ps1
-Restart-Computer
-
+# install Tableau 2023.2.2
+choco install tableau-desktop
