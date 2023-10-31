76adcc8b6da95530d1d9e5649e1a8fbb7a6069e3
Peter De Tender <peter@pdtit.be>
2023-10-31 10:50:31 -0700

    Create configure-winlabvm-anaconda.ps1

---
+++ AzlabJumpVM/configure-winlabvm-anaconda.ps1
@@ -0,0 +1,18 @@
+# Install Chocolatey
+
+# Set Chocolatey version to 1.4.0 to avoid installing .NET 4.8 Framework (which the newer 2.0 needs, but requires reboot, and blocks CustomScriptExtension
+
+$env:chocolateyVersion = '1.4.0'
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+## updates
+# allow all choco updates automatically 
+choco feature enable -n allowGlobalConfirmation
+
+# install Google Chrome
+choco install googlechrome
+
+# install Anaconda Distribution (Python 3.x) 2023.9.0
+choco install anaconda3
+
