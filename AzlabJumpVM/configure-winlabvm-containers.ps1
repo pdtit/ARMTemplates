03fca5cb4887b8c7291f3c0a9fe88be80dd553a7
Peter De Tender <peter@pdtit.be>
2023-10-31 08:37:24 -0700

    Create configure-winlabvm-containers.ps1

---
+++ AzlabJumpVM/configure-winlabvm-containers.ps1
@@ -0,0 +1,29 @@
+# Install Chocolatey
+
+# Set Chocolatey version to 1.4.0 to avoid installing .NET 4.8 Framework (which the newer 2.0 needs, but requires reboot, and blocks CustomScriptExtension
+
+$env:chocolateyVersion = '1.4.0'
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+## updates
+# allow all choco updates automatically 
+choco feature enable -n allowGlobalConfirmation
+
+# install Google Chrome
+choco install googlechrome
+
+# install Podman 
+choco install podman-machine
+choco install podman-desktop
+choco install podman-cli
+
+$ docker desktop
+choco install docker-desktop
+
+#install Docker on Windows Server
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
+.\install-docker-ce.ps1
+Restart-Computer
+
