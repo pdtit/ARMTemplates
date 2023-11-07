f1c88a9f6040807aa4c29dc0e1f8548d6f14f371
Peter De Tender <peter@pdtit.be>
2023-11-07 05:30:11 -0800

    Update configure-winlabvm-anaconda.ps1

--- AzlabJumpVM/configure-winlabvm-anaconda.ps1
+++ AzlabJumpVM/configure-winlabvm-anaconda.ps1
@@ -16,3 +16,8 @@ choco install googlechrome
 # install Anaconda Distribution (Python 3.x) 2023.9.0
 choco install anaconda3
 
+# install PostgreSQL
+choco install postgresql --params '/Password:P@55w.rdP@55w.rd /Port:5433' --ia '--enable-components server,commandlinetools'
+
+# install PGAdmin
+choco install pgadmin4
