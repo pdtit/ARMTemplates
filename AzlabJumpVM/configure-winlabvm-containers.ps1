c45c1dd5f6f0ca5511542c4eed5c7095a71bfce4
Peter De Tender <peter@pdtit.be>
2023-11-22 07:54:53 -0800

    Update configure-winlabvm-containers.ps1

--- AzlabJumpVM/configure-winlabvm-containers.ps1
+++ AzlabJumpVM/configure-winlabvm-containers.ps1
@@ -24,15 +24,11 @@ choco install podman-cli
 # visual studio code with Kubernetes Extension
 choco install vscode
 
-# Kubernetes tools
-#choco install kubernetes-helm **hangs setup
-
 #install Docker on Windows Server
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
 Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
 .\install-docker-ce.ps1
 
-#install Hyper-V on Windows Server
-Install-WindowsFeature -Name Hyper-V -ComputerName jumpvm -IncludeManagementTools -Restart
+Restart-Computer
 
 
