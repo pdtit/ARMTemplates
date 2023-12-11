9f82c040921980bdf306d201fac1ba59f944dba8
Peter De Tender <peter@pdtit.be>
2023-12-10 17:44:42 -0800

    Add files via upload

---
+++ AzlabJumpVM/HyperV/DSC/InstallHyperV.ps1
@@ -0,0 +1,24 @@
+﻿Configuration InstallHyperV
+{
+    Import-DscResource -Module xPendingReboot # Used for Reboots
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature 'Hyper-V'
+        {
+            Name   = 'Hyper-V'
+            Ensure = 'Present'
+            IncludeAllSubFeature = $true
+        }
+
+        xPendingReboot AfterRoleInstallation
+        {
+            Name       = 'AfterRoleInstallation'
+        }
+     }
+  }
