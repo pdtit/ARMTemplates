0316a216527c18296fe84604e415364da2d89230
Peter De Tender <peter@pdtit.be>
2024-01-04 15:17:46 -0800

    Update configure-winlabvm-appium.ps1

--- AzlabJumpVM/configure-winlabvm-appium.ps1
+++ AzlabJumpVM/configure-winlabvm-appium.ps1
@@ -11,7 +11,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco feature enable -n allowGlobalConfirmation
 
 # install Google Chrome
-choco install googlechrome
+choco install googlechrome --pre=120.0.6099.110 --ignore-checksums -y #pdt fixing an issue on Jan4 with new msi package
 
 # install Appium Server and Inspector GUI
 choco install appium-desktop
