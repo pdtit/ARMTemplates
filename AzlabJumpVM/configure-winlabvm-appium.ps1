edd6fd2c3891b57a7f311296dc304476f3c008f7
Peter De Tender <peter@pdtit.be>
2024-01-04 08:30:20 -0800

    Create configure-winlabvm-appium.ps1

---
+++ AzlabJumpVM/configure-winlabvm-appium.ps1
@@ -0,0 +1,14 @@
+# Install Chocolatey
+
+# Set Chocolatey version to 1.4.0 to avoid installing .NET 4.8 Framework (which the newer 2.0 needs, but requires reboot, and blocks CustomScriptExtension
+
+$env:chocolateyVersion = '1.4.0'
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+## updates
+# allow all choco updates automatically 
+choco feature enable -n allowGlobalConfirmation
+
+# install Google Chrome
+choco install googlechrome
