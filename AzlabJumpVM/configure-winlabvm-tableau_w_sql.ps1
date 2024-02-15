89030b981eee6f222f5cf9f447d9579711962940
Peter De Tender <peter@pdtit.be>
2024-02-14 16:22:47 -0800

    Update configure-winlabvm-tableau_w_sql.ps1

--- AzlabJumpVM/configure-winlabvm-tableau_w_sql.ps1
+++ AzlabJumpVM/configure-winlabvm-tableau_w_sql.ps1
@@ -28,3 +28,56 @@ choco install tableau-desktop
     # Copy the zip file from the temp path to the desktop path
     Copy-Item -Path $TempPath -Destination $DesktopPath
     #endofscript
+
+## restoring SQL Server AdventureWorks database
+        $logs = "G:\SQLLog"
+		$data = "F:\SQLData"
+		$backups = "F:\SQLTemp" 
+		
+		# Setup the data, backup and log directories as well as mixed mode authentication
+		#Import-Module "sqlps" -DisableNameChecking
+		Install-Module -Name SqlServer -RequiredVersion 21.1.18102 -AllowClobber -Force
+		[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
+		$sqlesq = new-object ('Microsoft.SqlServer.Management.Smo.Server') Localhost
+		$sqlesq.Settings.LoginMode = [Microsoft.SqlServer.Management.Smo.ServerLoginMode]::Mixed
+		$sqlesq.Settings.DefaultFile = $data
+		$sqlesq.Settings.DefaultLog = $logs
+		$sqlesq.Settings.BackupDirectory = $backups
+		$sqlesq.Alter() 
+
+		# Restart the SQL Server service
+		Restart-Service -Name "MSSQLSERVER" -Force
+		# Re-enable the sa account and set a new password to enable login
+		Invoke-Sqlcmd -ServerInstance Localhost -Database "master" -Query "ALTER LOGIN sa ENABLE"
+		Invoke-Sqlcmd -ServerInstance Localhost -Database "master" -Query "ALTER LOGIN sa WITH PASSWORD = 'P@55w.rdP@55w.rd'"
+
+		# Download the Microsoft.eShopOnWeb.CatalogDb database backup 
+		$dbsource = "https://azlabstudentsstor.blob.core.windows.net/labartifacts/AdventureWorks2022.bak"
+		$dbbackupfile = "F:\SQLDATA\AdventureWorks2022.bak"
+		$dbdestination = "F:\SQLDATA\AdventureWorks2022.bak"
+
+		Invoke-WebRequest $dbsource -OutFile $dbdestination -UseBasicParsing
+
+		# Define parameters for the actual restore
+		#RelocateData = sets the location for the database
+		#RelocateLog = sets the location for the logfiles
+		#$file = sets the parameter to both database and logfiles
+		#$myarr = data and logfile is stored as an array, which is picked up by the restore-sqldatabase PowerSHell cmd
+
+		#try without relocate 
+		#$RelocateData = New-Object 'Microsoft.SqlServer.Management.Smo.RelocateFile, Microsoft.SqlServer.SmoExtended, Version=12.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91' -ArgumentList "Microsoft.eShopOnWeb.CatalogDb2014_Data", "F:\Data\Microsoft.eShopOnWeb.CatalogDbDB.mdf"
+		#$RelocateLog = New-Object 'Microsoft.SqlServer.Management.Smo.RelocateFile, Microsoft.SqlServer.SmoExtended, Version=12.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91' -ArgumentList "Microsoft.eShopOnWeb.CatalogDb2014_Log", "F:\Logs\Microsoft.eShopOnWeb.CatalogDbDB.ldf"
+		#$file = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile($RelocateData,$RelocateLog) 
+		#$myarr=@($RelocateData,$RelocateLog)
+
+		#run the actual database restore
+		#original cmdlet Restore-SqlDatabase -ServerInstance Localhost -Database "Microsoft.eShopOnWeb.CatalogDb" -BackupFile $dbbackupfile -RelocateFile $myarr
+		
+		#Restore-SqlDatabase -ServerInstance Localhost -Database "Microsoft.eShopOnWeb.CatalogDb" -RestoreAction Database -BackupFile $dbbackupfile 
+		Restore-SqlDatabase -ServerInstance Localhost -Database "AdventureWorks2022" -BackupFile $dbbackupfile -AutoRelocateFile -PassThru
+
+
+		#allow connection to SQL Instance
+		New-NetFirewallRule -DisplayName "SQL Server" -Direction Inbound -Protocol TCP -LocalPort 1433 -Action allow 
+		
+	
